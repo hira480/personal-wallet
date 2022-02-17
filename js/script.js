4931f3ce1cbd00db4c5fdd2a926157647aa60ce6
hira480 <hiramannan8@gmail.com>
2022-02-17 20:42:55 +0600

    percentage and remaining balance added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,40 @@
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const income = document.getElementById('income-field');
-    const income1 = income.value
-    const food = document.getElementById('food-expense');
-    const food1 = food.value;
-    const rent = document.getElementById('rent-expense');
-    const rent1 = rent.value;
-    const clothes = document.getElementById('cloth-expense');
-    const clothes1 = clothes.value;
-    const totalExpenses = food1 + rent1 + clothes1;
-    const newExpense = document.getElementById('total-expenses').innerText;
-    newExpense.innerText = totalExpenses;
+    const incomeField = document.getElementById('income-field');
+    const income = parseInt(incomeField.value);
+    const foodField = document.getElementById('food-expense');
+    const food = parseInt(foodField.value);
+    const rentField = document.getElementById('rent-expense');
+    const rent = parseInt(rentField.value);
+    const clothsField = document.getElementById('cloth-expense');
+    const cloths = parseInt(clothsField.value);
+    const expenses = food + rent + cloths;
 
-    const balance = income - totalExpenses;
-    const newBalance = document.getElementById('balence').innerText;
-    newBalance.innerText = balance;
-    return totalExpenses;
-})
+    const totalExpenseText = document.getElementById('total-expenses');
+    const totalExpenses = totalExpenseText.innerText;
+    totalExpenseText.innerText = expenses;
+
+    const balanceTotal = income - expenses;
+
+    const balanceText = document.getElementById('balence');
+    const balanceSum = balanceText.innerText;
+    balanceText.innerText = balanceTotal;
+
+    // console.log(percentageAmount);
+    document.getElementById('saving-btn').addEventListener('click', function () {
+        const percentageField = document.getElementById('saving-percentage');
+        const percentageAmount = parseInt(percentageField.value);
+
+        const percentage = (balanceTotal * percentageAmount) / 100;
+
+        const savingAmountText = document.getElementById('saving-amount');
+        const savingAmount = savingAmountText.innerText;
+        savingAmountText.innerText = percentage;
+
+        const remainingBalance = balanceTotal - percentage;
+
+        const remainingBalanceText = document.getElementById('remaining-balance');
+        const finalAmount = remainingBalanceText.innerText;
+        remainingBalanceText.innerText = remainingBalance;
+        // console.log(remainingBalance);
+    });
+});
